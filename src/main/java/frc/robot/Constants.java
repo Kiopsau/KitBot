cbe4a8bf03a77200a8e0fb97739a688843c19523
Kiopsau <28NickS2010@gmail.com>
2025-01-24 17:40:34 -0500

    wow!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,7 +3,7 @@ package frc.robot;
 public final class Constants {
     public final class ShooterConstants {
         public static final int CoralPort = 3; 
-        public static final double CoralSpeed = 1.0; 
+        public static final double CoralSpeed = -8.0; 
     }
 
     public final class DriverConstants {
